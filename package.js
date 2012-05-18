b158eb03ea1c831ce6f7e9301bfc143c7fb09579
Martin Loy <martinloy.uy@gmail.com>
2012-05-18 19:52:28 -0300

    ultima version.. non working

--- package.js
+++ package.js
@@ -4,5 +4,6 @@
 	"dependecines": {
 		"express": "2.5.x",
 		"jade": "0.26.0",
+		"mongoose": "2.6.x",
 	}
 }
--- server.js
+++ server.js
@@ -1,28 +1,44 @@
-var express = require('express');
-var server = express.createServer();
-var fs = require('fs');
+var express = require('express')
+,   mongoose = require('mongoose')
+,   server = express.createServer();
 
 /**
     Configuration
 **/
+mongoose.connect('mongodb://localhost/bla');
 server.set('view_engine', 'jade');
-server.set('views', _dirname + '/views');
+server.set('views', __dirname + '/views');
 server.set('view options', { layout: false});
 
+
+var User = mongoose.model("User", new Schema({
+        name: {first: String, last: String},
+        user: String,
+        pass: String,
+    })
+);
+o
+User.statics.authenticate = function ( user, pass, fn) {
+    this.findOne({ 'user': user, 'pass': pass}), function (err, doc){
+        if (err) return fn(err);
+    }
 /**
     App init
 **/
 server.use(express.bodyParser());
 server.use(express.cookieParser());
-server.use(express.session());
+server.use(express.session({secret: 'asdasdas'}));
 
 server.get('/', function (req, res, next){
-    res.render('index', { authenticated: false});
+    res.render('index', { authenticated: req.session.authenticated});
 });
 
 server.post('/login', function (req, res, next) {
-    fs.readFile(_dirname + 'users.json', function(err, data){
+    fs.readFile(__dirname + 'users.json', function(err, data){
         var users = JSON.parse(data);
+        if (users[req.body.user] == req.body.pass){
+        t   req.session.authenticated = true;
+        }
     });
 });
 
