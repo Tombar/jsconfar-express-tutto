812070214cd8f6b360168d52a8535716c2dc2f08
Martin Loy <martinloy.uy@gmail.com>
2012-05-18 18:43:56 -0300

    version 3 vamos armando aglo con users

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var server = express.createServer();
+var fs = require('fs');
 
 /**
     Configuration
@@ -8,8 +9,21 @@ server.set('view_engine', 'jade');
 server.set('views', _dirname + '/views');
 server.set('view options', { layout: false});
 
+/**
+    App init
+**/
+server.use(express.bodyParser());
+server.use(express.cookieParser());
+server.use(express.session());
+
 server.get('/', function (req, res, next){
     res.render('index', { authenticated: false});
+});
+
+server.post('/login', function (req, res, next) {
+    fs.readFile(_dirname + 'users.json', function(err, data){
+        var users = JSON.parse(data);
+    });
 });
 
 module.exports = server;
